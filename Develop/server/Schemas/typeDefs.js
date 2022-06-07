c512de810ffe401e36d9a2baa397bb7e760062e7
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-06-07 07:35:53 -0500

    added type Mutation to typeDefs

--- Develop/server/Schemas/typeDefs.js
+++ Develop/server/Schemas/typeDefs.js
@@ -2,6 +2,16 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
-    me: User
+    me: user
+  }
+  
+  type Mutation {
+    login(email: string, password: string): auth
+
+    addUser(username: string, email: string, password: string): auth
+
+    saveBook(newBook: InputBook): user
+
+    removeBook(bookId: id): user
   }
 `;
